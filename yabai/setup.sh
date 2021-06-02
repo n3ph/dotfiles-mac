4f98e58ef4a59bed2d2f93f3b05defb0801b9114
Michael Gerlach <michael.gerlach@commercetools.com>
2021-06-02 22:13:05 +0200

    Add yabai
    Signed-off-by: Michael Gerlach <michael.gerlach@commercetools.com>

---
+++ yabai/setup.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+. ../scripts/functions.sh
+
+SOURCE="$(realpath -m .)"
+DESTINATION="$(realpath -m ~)"
+
+info "Configuraing yabai..."
+
+find .* -type f | while read fn; do
+    fn=$(basename $fn)
+    symlink "$SOURCE/$fn" "$DESTINATION/$fn"
+done
+
+success "Finished configuring yabai."
